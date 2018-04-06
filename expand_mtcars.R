c42f191a457d0a5e823f64145a93c5ea2d2387b4
larcat <larcatdm@gmail.com>
2018-04-05 22:01:37 -0500

    Mods and tickets added

--- expand_mtcars.R
+++ expand_mtcars.R
@@ -1,8 +1,17 @@
+#### Desc ####
+# Street racers are a plague on the nation!
+# Recently, many street racers have gotten speeding tickets in both modified and
+# unmodified cars. Furthermore, cars impounded at street racing events have been
+# weighed and put on the dyno.
+# This script gives you info about those cars, their modifications and those 
+# tickets.
+
 #### Dependencies ####
 library(dplyr)
 library(readxl)
 library(tidycensus)
 library(tidyr)
+library(XML)
 
 #### State fips location ####
 # https://www2.census.gov/geo/docs/reference/codes/national_county.txt
@@ -60,7 +69,65 @@ library(tidyr)
       return(final_df)
     }
     
-  #### Add ticket data to long states data frame ####
-    
-    
+  #### Add modification noise to long states data frame ####
+    # Creates random vairable, assigns mods according to that.
+    # Increases hp, decreases weight.
+    add_mods <- function(df){
+      
+      df <- df %>%
+        mutate(mod_num = rnorm(n = n()),
+               hp_num = rnorm(n = n(), mean = 1.2, sd = .05),
+               wt_num = rnorm(n = n(), mean = .9, sd = .05),
+               mod_flag = ifelse(mod_num <= 1.5 & mod_num >= -1.5, FALSE, TRUE),
+               hp = ifelse(mod_flag == TRUE, hp * hp_num, hp),
+               wt_lbs = ifelse(mod_flag == TRUE, wt_lbs * wt_num, wt_lbs),
+               hp = round(hp, 0),
+               wt_lbs = round(wt_lbs, 0)) %>%
+        select(-mod_num, -hp_num, -wt_num)
+      
+      return(df)
+    }
   
+  #### Add tickets to long states data frame ####
+    # Pulls speed limits from wikipedia.
+    # Imputs the ones with annoying embedded chars as the mode (70)
+    # Selects ticketed cars based on hp/lb ratio
+    # Imputes speed of ticket based on speed limit + hp/lb ratio
+    # Imputes price of ticket based on num rows, rounded to 100
+    add_tickets <- function(df, 
+                            speed_lim_loc = "https://en.wikipedia.org/wiki/Speed_limits_in_the_United_States"){
+      
+      speed_lim_table <- htmltab(speed_lim_loc, 2) %>%
+        select(`State or territory`,
+               `Freeway (rural)`) %>%
+        rename(state = `State or territory`,
+               speed_lim = `Freeway (rural)`) %>%
+        mutate(speed_lim = as.numeric(substr(speed_lim, 1, 2)),
+               speed_lim = ifelse(is.na(speed_lim), 70, speed_lim))
+      
+      df <- df %>% left_join(speed_lim_table) %>%
+        mutate(ratio = hp / wt_lbs,
+               ticket_num = rnorm(n = n(), 
+                                  mean = mean(ratio),
+                                  sd = sd(ratio)),
+               ticket_flag = ifelse(ticket_num < .6 * ratio, TRUE, FALSE),
+               ticket_amount_num = rnorm(n = n(),
+                                         mean = 1000,
+                                         sd = 100),
+               ticket_amount = ifelse(ticket_flag == TRUE, 
+                                      round(ticket_amount_num, 20),
+                                      0)) %>%
+        select(-ticket_num, -ticket_flag, - ticket_amount_num, -ratio)
+      
+      return(df)
+    }
+    
+  #### Long form wrapper function ####
+    # Outputs long form data set, pre fracturing
+    long_output <- function(){
+      df <- state_rows() %>%
+        add_mods() %>%
+        add_tickets()
+      
+      return(df)
+    }
